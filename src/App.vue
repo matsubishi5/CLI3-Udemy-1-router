1a08a90dfa0e1c6f89e1e008f9b3b73483ba10d1
TakumiMatsuo <user@matsuotakumi-no-MacBook-Air.local>
2020-06-09 17:19:35 +0900

    [learn]router active-class,exact

--- src/App.vue
+++ src/App.vue
@@ -1,9 +1,15 @@
 <template>
   <div>
     <nav>
-      <router-link to="/">Home</router-link>
-      <router-link to="/users">Users</router-link>
+      <router-link to="/" active-class="link--active" exact>Home</router-link>
+      <router-link to="/users" active-class="link--active" exact>Users</router-link>
     </nav>
     <router-view></router-view>
   </div>
 </template>
+
+<style>
+.link--active {
+  font-size: 30px;
+}
+</style>
