1b28bfb2d24b09fb2e776e22a0d6494d120bb3b6
TakumiMatsuo <user@matsuotakumi-no-MacBook-Air.local>
2020-06-09 20:42:59 +0900

    [learn]router-view transition

--- src/App.vue
+++ src/App.vue
@@ -1,12 +1,19 @@
 <template>
   <div>
     <router-view name="header"></router-view>
-    <router-view></router-view>
+    <transition name="fade" mode="out-in">
+      <router-view></router-view>
+    </transition>
   </div>
 </template>
 
-<style>
-.link--active {
-  font-size: 30px;
+<style scoped>
+.fade-enter,
+.fade-leave-to {
+  opacity: 0;
+}
+.fade-enter-active,
+.fade-leave-active {
+  transition: opacity 0.5s;
 }
 </style>
